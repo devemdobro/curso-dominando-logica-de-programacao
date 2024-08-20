fe2ded401d31c6466d47f1580b825b817c79fe9f
devemdobro <devemdobro@gmail.com>
2024-08-20 12:15:15 -0300

    correção de identação

--- big-o-notation-2/index.js
+++ big-o-notation-2/index.js
@@ -9,10 +9,10 @@ function copyArray(arr) {
 }
 
 function sumToN(n) {
-    if (n <= 0) return 0;
-    
-    return n + sumToN(n - 1);
-  }
-  
-  const result = sumToN(5); // 5 + 4 + 3 + 2 + 1
-  console.log(result);
+  if (n <= 0) return 0;
+
+  return n + sumToN(n - 1);
+}
+
+const result = sumToN(5); // 5 + 4 + 3 + 2 + 1
+console.log(result);
